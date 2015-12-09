0cda5ffaf386f27a882e822d21c4e9a2f3ee0b21
Nickolay Polyarniy <PolarNick239@gmail.com>
2015-12-09 23:26:10 +0300

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,29 @@
+sudo apt-get install aptitude
+sudo aptitude install nvidia-<VERSION> nvidia-modprobe clinfo opencl-headers mesa-utils
+sudo aptitude install chromium-browser gparted htop krusader vlc p7zip-rar p7zip-full rar cmake-qt-gui git-cola
+sudo aptitude install system-config-lvm
+
 sudo update-alternatives --set x-www-browser /usr/bin/chromium-browser
+
+sudo aptitude install git python3-pip
+
+# telegram
+# install skype - http://tipsonubuntu.com/2015/03/24/install-skype-4-3-in-ubuntu-15-04/
+
+# subl
+# sudo add-apt-repository ppa:webupd8team/sublime-text-3
+# sudo apt-get update && sudo apt-get install sublime-text-installer
+# sudo add-apt-repository -r ppa:webupd8team/sublime-text-3
+
+sudo apt-get install indicator-multiload
+
+sudo aptitude install compizconfig-settings-manager unity-tweak-tool gpointing-device-settings
+
+# sudo lvresize -L +10G local/root
+# sudo resize2fs /dev/mapper/local-root
+
+sudo apt-get install openjdk-8-jdk openjdk-8-doc openjdk-8-source 
+sudo apt-get install python3-virtualenv
+
+sudo apt-get install i3
+sudo pip3 install i3pystatus  # http://askubuntu.com/questions/598943/how-to-de-uglify-i3-wm
