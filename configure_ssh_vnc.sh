4e3102d140ed292a1dcfd0e3e0e21b13e394cabf
Nikolai Poliarnyi <PolarNick239@gmail.com>
2023-03-12 10:49:47 +0000

    Update configure_ssh_vnc.sh

--- configure_ssh_vnc.sh
+++ configure_ssh_vnc.sh
@@ -11,13 +11,16 @@ sudo ufw enable && sudo ufw reload
 # X11VNC
 sudo apt build-dep x11vnc # may require to enable repos in GUI - https://askubuntu.com/a/857433
 git clone https://github.com/LibVNC/x11vnc
-git checkout 0c4fbc3cf69471bc1288a5f8d2a218e798d01cff
 cd x11vnc
+git checkout 0c4fbc3cf69471bc1288a5f8d2a218e798d01cff
 autoreconf -fiv
 ./configure
 make
 sudo make install
+cd ..
 rm -rf x11vnc
+sudo ufw allow ${vnc_port}/tcp # 5900 by default
+sudo ufw enable && sudo ufw reload
 
 DISPLAY=:0 x11vnc -usepw -noncache -wait 5 -defer 5
 
