c711e3b49d1475bd37af67dae76603201eda9fa6
Nikolai Poliarnyi <PolarNick239@gmail.com>
2023-03-12 16:24:25 +0300

    Update configure_ssh_vnc.sh

--- configure_ssh_vnc.sh
+++ configure_ssh_vnc.sh
@@ -13,9 +13,26 @@ sudo apt build-dep x11vnc # may require to enable repos in GUI - https://askubun
 git clone https://github.com/LibVNC/x11vnc
 cd x11vnc
 git checkout 0c4fbc3cf69471bc1288a5f8d2a218e798d01cff
+
+# IF with TurboJPEG + TurboVNC:
+# https://github.com/LibVNC/x11vnc/blob/master/doc/FAQ.md#q-80-does-x11vnc-support-turbovnc
+# https://github.com/LibVNC/x11vnc/blob/master/misc/turbovnc/README
+cd ..
+git clone https://github.com/LibVNC/libvncserver
+cd libvncserver
+git checkout 111db4bea124af27986c665dabd30c290168bb3d
+cp src/libvncserver/* . # we need tight.c and rfbserver.c
+cd ../x11vnc/misc/turbovnc
+./apply_turbovnc
+cd ../..
+sudo apt install libturbojpeg
 autoreconf -fiv
-./configure
-make
+env LDFLAGS='-L/usr/lib/x86_64-linux-gnu/ -Xlinker --rpath=/usr/lib/x86_64-linux-gnu/' ./configure
+make AM_LDFLAGS='-lturbojpeg'
+# IFELSE without TurboJPEG + TurboVNC:
+#autoreconf -fiv
+#./configure
+#make
 sudo make install
 cd ..
 rm -rf x11vnc
@@ -23,6 +40,7 @@ sudo ufw allow ${vnc_port}/tcp # 5900 by default
 sudo ufw enable && sudo ufw reload
 
 DISPLAY=:0 x11vnc -usepw -noncache -wait 5 -defer 5
+
 
 
 # Client System:
@@ -33,3 +51,8 @@ sudo dpkg -i turbovnc*.deb
 rm turbovnc*.deb
 
 /opt/TurboVNC/bin/vncviewer ${ip}
+
+# Remmina for RDP
+sudo apt-add-repository ppa:remmina-ppa-team/remmina-next
+sudo apt update
+sudo apt install remmina remmina-plugin-rdp remmina-plugin-secret
